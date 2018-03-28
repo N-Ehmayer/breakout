a547a574aae0d2f6d8c6b45b495a5688bb53da0d
N-Ehmayer <nic.ehmayer92@gmail.com>
2018-03-27 18:02:05 -0600

    Set up canvas

--- game.js
+++ game.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+var canvas;
+var canvasContext;
+
+window.onload = function() {
+  console.log("Hello World!");
+  canvas = document.getElementById('gameCanvas');
+  canvasContext = canvas.getContext('2d');
+  canvasContext.fillStyle = 'black';
+  canvasContext.fillRect(0,0,canvas.width,canvas.height);
+}
