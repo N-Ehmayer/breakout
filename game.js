b5114ec1812e605a24e923851c971f69f780461c
N-Ehmayer <nic.ehmayer92@gmail.com>
2018-03-27 18:51:22 -0600

    Added basic movement block

--- game.js
+++ game.js
@@ -1,10 +1,26 @@
-var canvas;
-var canvasContext;
+let canvas = {};
+let canvasContext = {};
+
+let ballX = 50;
+
 
 window.onload = function() {
   console.log("Hello World!");
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
+
+  const framesPerSecond = 30;
+  setInterval(drawEverything, 1000/framesPerSecond);
+}
+
+function drawEverything() {
+  ballX = ballX + 5;
+
   canvasContext.fillStyle = 'black';
-  canvasContext.fillRect(0,0,canvas.width,canvas.height);
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  canvasContext.fillStyle = 'blue';
+  canvasContext.fillRect(100, 0, 100, 25);
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(ballX, 100, 10, 10);
+
 }
