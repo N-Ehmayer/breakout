a27bbc9ae67c8caeea2a5e60c2e939d99943b626
N-Ehmayer <nic.ehmayer92@gmail.com>
2018-04-02 18:02:56 -0600

    Initial commit

--- game.js
+++ game.js
@@ -1,200 +1,140 @@
-let canvas;
-let canvasContext;
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-let ballX = 50;
-let ballY = 50;
-let ballSpeedX = 10;
-let ballSpeedY = 5;
+const ball = new Image();
+ball.src = './assets/images/ball.png';
+const ballDi = 25;
+let ballXPos = canvas.width/2;
+let ballYPos = canvas.height - 60;
+let ballXSpeed = 2;
+let ballYSpeed = -4;
 
-let paddle1Y = 250;
-let paddle2Y = 250;
-const PADDLE_HEIGHT = 100;
-const PADDLE_WIDTH = 10;
-const WALL_PADDING = 10;
+const paddle = new Image();
+paddle.src = './assets/images/paddle.png';
+const paddleHeight = 25;
+const paddleWidth = 100;
+const paddlePadding = 5;
+let paddleX = (canvas.width-paddleWidth)/2;
 
-let player1Score = 0;
-let player2Score = 0;
-const WINNING_SCORE = 3;
+let rightPressed = false;
+let leftPressed = false;
 
-let showingWinScreen = true;
+const heart = new Image();
+heart.src = './assets/images/heart.png';
+const heartWidth = 30;
+const heartHeight = 28;
+const heartPadding = 10;
+let hearts = [1, 1, 1];
 
-const hitSound = new Audio('assets/square_beep.wav');
-const missSound = new Audio('assets/square_miss.wav');
-const winSound = new Audio('assets/square_win.wav');
-const loseSound = new Audio('assets/square_lose.wav');
-
-
-function playSoundEffect(sound) {
-  if (typeof prevSound === 'undefined') {
-    let prevSound = sound;
-    sound.play();
-  } else {
-    prevSound.pause();
-    prevSound.currentTime = 0;
-    sound.play();
-    prevSound = sound;
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+const bricks = [];
+for (c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0 };
   }
 }
+console.log(bricks);
 
-function handleMouseClick(evt) {
-  if (showingWinScreen) {
-    player1Score = 0;
-    player2Score = 0;
-    showingWinScreen = false;
+function drawBall() {
+  ctx.beginPath();
+  // ctx.arc(ballXPos, ballYPos, ballRadius, 0, Math.PI*2);
+  ctx.drawImage(ball, ballXPos, ballYPos, ballDi, ballDi)
+  ctx.closePath();
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  // ctx.rect(paddleX, canvas.height-paddleHeight-paddlePadding, paddleWidth, paddleHeight);
+  ctx.drawImage(paddle, paddleX, canvas.height-paddleHeight-paddlePadding, paddleWidth, paddleHeight);
+  // ctx.fillStyle = "#0095DD";
+  // ctx.fill();
+  ctx.closePath();
+}
+
+function drawHearts() {
+  let heartXPos = 20;
+  ctx.beginPath();
+  for (h = 0; h < hearts.length; h++) {
+    ctx.drawImage(heart, heartXPos, 20, heartWidth, heartHeight);
+    heartXPos += 35;
   }
+  ctx.closePath();
 }
 
-window.onload = function() {
-  console.log("Hello World!");
-  canvas = document.getElementById('gameCanvas');
-  canvasContext = canvas.getContext('2d');
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  drawPaddle();
+  drawHearts();
 
-  const framesPerSecond = 30;
-  setInterval(function() {
-    moveEverything();
-    drawEverything();
-  }, 1000/framesPerSecond);
+  // Left and right wall collision
+  if (ballXPos < 0 || ballXPos + ballDi > canvas.width) {
+    ballXSpeed = -ballXSpeed;
+  }
 
-  canvas.addEventListener('mousedown', handleMouseClick)
+  // Top wall collision
+  if (ballYPos + ballYSpeed < 0) {
+    ballYSpeed = -ballYSpeed;
+  }
 
-  canvas.addEventListener('mousemove', function(evt) {
-    const mousePos = calculateMousePos(evt);
-    paddle1Y = mousePos.y;
-  })
-}
+  // Paddle and ground collision
+  if (ballYPos > canvas.height - ballDi - paddleHeight - paddlePadding) {
+    if (ballXPos > paddleX && ballXPos < paddleX + paddleWidth && ballYPos < canvas.height-paddleHeight-paddlePadding) {
+      ballYSpeed = -ballYSpeed;
+    } else if (ballYPos > canvas.height) {
+      ballReset();
+    }
+  }
 
-function calculateMousePos(evt) {
-  const rect = canvas.getBoundingClientRect();
-  const root = document.documentElement;
-  const mouseX = evt.clientX - rect.left - root.scrollLeft;
-  const mouseY = evt.clientY - rect.top - root.scrollTop;
-  return {
-    x: mouseX,
-    y: mouseY
-  };
+  ballXPos += ballXSpeed;
+  ballYPos += ballYSpeed;
+
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 5;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 5;
+  }
 }
 
 function ballReset() {
-  if (player1Score >= WINNING_SCORE) {
-    showingWinScreen = true;
-    playSoundEffect(winSound);
-  } else if (player2Score >= WINNING_SCORE) {
-    showingWinScreen = true;
-    playSoundEffect(loseSound);
+  if (hearts.length <= 1) {
+    alert('GAME OVER');
+    hearts = [1, 1, 1];
   } else {
-    playSoundEffect(missSound);
+    hearts.pop();
   }
 
-  if (ballX < 0 || ballX > canvas.width) {
-    ballSpeedX = -ballSpeedX;
-    ballX = canvas.width / 2;
-    ballY = canvas.height / 2;
+  ballXPos = paddleX + paddleWidth/2;
+  ballYPos = canvas.height - 60;
+  ballXSpeed = 2;
+  ballYSpeed = -4;
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = true;
+  } else if (e.keyCode == 37) {
+    leftPressed = true;
   }
 }
 
-function computerMovement() {
-  let paddle2YCenter = paddle2Y + (PADDLE_HEIGHT  / 2);
-  if (paddle2YCenter < ballY - 35) {
-    paddle2Y += 10;
-  } else if (paddle2YCenter > ballY + 35) {
-    paddle2Y -= 10;
+function keyUpHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = false;
+  } else if (e.keyCode == 37) {
+    leftPressed = false;
   }
 }
 
-function moveEverything() {
-  if(showingWinScreen) {
-    return;
-  }
-  computerMovement();
+setInterval(draw, 10);
 
-  ballX += ballSpeedX;
-  ballY += ballSpeedY;
-
-  if (ballX < PADDLE_WIDTH + WALL_PADDING + 10) {
-    console.log("Test")
-    if (ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT) {
-      ballSpeedX = -ballSpeedX;
-      let deltaY = ballY - (paddle1Y + PADDLE_HEIGHT / 2);
-      ballSpeedY = deltaY * 0.30;
-      playSoundEffect(hitSound);
-    } else if (ballX < 0) {
-      player2Score++; // Must be before ballReset()
-      ballReset();
-    }
-  }
-  if (ballX > canvas.width - (WALL_PADDING + PADDLE_WIDTH + 10)) {
-    if (ballY > paddle2Y && ballY < paddle2Y + PADDLE_HEIGHT) {
-      ballSpeedX = -ballSpeedX;
-      let deltaY = ballY - (paddle2Y + PADDLE_HEIGHT / 2);
-      ballSpeedY = deltaY * 0.30;
-      playSoundEffect(hitSound);
-    } else if (ballX > canvas.width) {
-      player1Score++;
-      ballReset();
-    }
-  }
-  if (ballY < 10) {
-    ballSpeedY = -ballSpeedY;
-  }
-  if (ballY > canvas.height - 10) {
-    ballSpeedY = -ballSpeedY;
-  }
-}
-
-function drawNet() {
-  for (let i = 10; i < canvas.height; i += 40) {
-    colorRect(canvas.width / 2 - 1, i, 2, 20, 'grey');
-  }
-}
-
-function drawEverything() {
-  // Canvas
-  colorRect(0, 0, canvas.width, canvas.height, 'black');
-
-  // Start screen
-  if (showingWinScreen) {
-    canvasContext.font = '80px serif'
-    if (player1Score >= WINNING_SCORE) {
-      canvasContext.fillStyle = 'green';
-      canvasContext.fillText('VICTORY', 225, 250);
-    } else if (player2Score >= WINNING_SCORE) {
-      canvasContext.fillStyle = 'red';
-      canvasContext.fillText('DEFEAT', 256, 250);
-    } else {
-      canvasContext.fillStyle = 'white';
-      canvasContext.fillText('Definitely not Pong', 85, 250);
-    }
-    canvasContext.font = '45px serif'
-    canvasContext.fillStyle = 'white';
-    canvasContext.fillText('click to continue', 250, 400);
-    return;
-  }
-
-  // Scoreboard
-  canvasContext.font = '45px sans-serif'
-  canvasContext.fillStyle = 'white';
-  canvasContext.fillText(player1Score, 190, 70);
-  canvasContext.fillText(player2Score, 590, 70);
-
-  drawNet();
-
-  // Left paddle
-  colorRect(WALL_PADDING, paddle1Y, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
-  // Right paddle
-  colorRect(canvas.width - PADDLE_WIDTH - WALL_PADDING, paddle2Y, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
-  // Ball
-  colorCircle(ballX, ballY, 10, 'white');
-
-}
-
-function colorCircle(centerX, centerY, radius, drawColor) {
-  canvasContext.fillStyle = drawColor;
-  canvasContext.beginPath();
-  canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
-  canvasContext.fill();
-}
-
-function colorRect(leftX, topY, width, height, drawColor) {
-  canvasContext.fillStyle = drawColor;
-  canvasContext.fillRect(leftX, topY, width, height);
-}
